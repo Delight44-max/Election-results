dfd72411596208947fc47431bdb1c4d266540cb7
Delight44-max <eboagorojahdiebube@gmail.com>
2026-08-13 15:51:35 +0100

    updated frontend url

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,7 +5,7 @@ dotenv.config();
 export const env = {
   PORT: parseInt(process.env.PORT || "10000", 10),
   DATABASE_URL: process.env.DATABASE_URL || "",
-  FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:3000",
+  FRONTEND_URL: process.env.FRONTEND_URL || "https://github.com/Delight44-max/Election-results.git",
   NODE_ENV: process.env.NODE_ENV || "development",
 };
 
